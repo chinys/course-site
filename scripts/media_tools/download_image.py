a05857f821f8a23c2afc97cb313b25e70a96aa3b
chinys <chinys00@gmail.com>
2026-03-07 15:22:00 +0900

    빌드만공유

--- scripts/media_tools/download_image.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import os
-
-os.makedirs('static/images', exist_ok=True)
-url = 'https://images.unsplash.com/photo-1555066931-4365d14bab8c?auto=format&fit=crop&q=80&w=800&h=600'
-urllib.request.urlretrieve(url, 'static/images/fastapi_thumbnail.png')
-print("Image downloaded successfully!")
